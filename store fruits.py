ac630d13f3463355f9042d5edc93ed60a4effd15
joecapon11-spec <joecapon11@gmail.com>
2026-06-30 19:33:41 +0100

    Add files via upload
    fruit list

---
+++ store fruits.py
@@ -0,0 +1,6 @@
+fruits = ("apple", "banana", "orange", "grape", "kiwi")
+enter_fruit = input("Enter a fruit name: ")
+if enter_fruit in fruits:
+    print("Fruit is in the list.")
+else:
+    print("Fruit is not in the list.")
